44a8167ca6c7589c3c2f85be4b1553720c9e8ec0
yongggnd <yongkerkwith@gmail.com>
2022-09-25 01:23:08 +0700

    feat bottom navigation using getx statemanagement

--- lib/controllers/app_controllers.dart
+++ lib/controllers/app_controllers.dart
@@ -2,6 +2,7 @@ import 'package:get/get.dart';
 
 class AppControllers extends GetxController {
   RxInt counter = 0.obs;
+  RxInt navIndex = 0.obs;
 
   increment() => {counter++};
 }
--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_getx/controllers/app_controllers.dart';
+import 'package:flutter_getx/pages/chat.dart';
+import 'package:flutter_getx/pages/feed.dart';
+import 'package:flutter_getx/pages/profile.dart';
 import 'package:get/get.dart';
 
 class HomePage extends StatelessWidget {
@@ -9,23 +12,40 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Counter'),
-      ),
-      body: Center(
-        child: Obx(
-          () => Text(
-            '${_controller.counter}',
-            style: Theme.of(context).textTheme.headline1,
-          ),
+    return GetX<AppControllers>(
+      init: AppControllers(),
+      builder: (controller) => Scaffold(
+        appBar: AppBar(
+          title: const Text('Counter'),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          _controller.increment();
-        },
-        child: const Icon(Icons.add),
+        body: IndexedStack(
+          index: _controller.navIndex.value,
+          children: const [
+            FeedPage(),
+            ChatPage(),
+            ProfilePage(),
+          ],
+        ),
+        bottomNavigationBar: NavigationBar(
+          selectedIndex: _controller.navIndex.value,
+          onDestinationSelected: ((value) {
+            _controller.navIndex.value = value;
+          }),
+          destinations: const [
+            NavigationDestination(
+              icon: Icon(Icons.feed),
+              label: 'Feed',
+            ),
+            NavigationDestination(
+              icon: Icon(Icons.chat),
+              label: 'Chat',
+            ),
+            NavigationDestination(
+              icon: Icon(Icons.account_circle),
+              label: 'Profile',
+            ),
+          ],
+        ),
       ),
     );
   }
